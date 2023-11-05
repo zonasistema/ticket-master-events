3e5154a4cb391b5eb0d89494d714fd164571292f
zonasistema <hector.flores@zonasistema.com>
2023-11-05 12:50:13 -0300

    Clase 5 Manejando Eventos

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,9 +1,10 @@
-const EventItem = ( { info, name, image, onEventClick }) => {
+const EventItem = ( { info, id, name, image, onEventClick }) => {
     return (
-        <div>
+        <div onClick={() => onEventClick(id)}>
             <img src={image} alt={name} width={200} height={200}/>
             <h4>{name}</h4>
             <p>{info}</p>
+            <button onClick={() => onEventClick(id)}>Ver mas</button>
         </div>
     );
 };
